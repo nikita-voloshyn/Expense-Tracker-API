a27d269f5517e80c2ae71aedc3b24f41213fb5c6
Nikita Voloshyn <nikita.voloshyn.odessa@gmail.com>
2025-06-19 18:06:34 +0300

    add models

--- core/apps/models.py
+++ core/apps/models.py
@@ -12,3 +12,12 @@ class Expense(models.Model):
         ('health', 'Health'),
         ('others', 'Others'),
     ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='expenses')
+    title = models.CharField(max_length=255)
+    anount = models.IntegerField()
+    category = models.CharField(max_length=20, choices=CategoryChoices)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.anount}"
